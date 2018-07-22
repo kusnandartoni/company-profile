9e462ed3fa184768f8b062bdf2c6b0bce58e386b
kusnandartoni <kusnandartoni@gmail.com>
2018-07-22 14:45:45 +0700

    add alert

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-// import Alert from './components/Alert';
-// import Card from './components/Card';
+import Alert from './components/Alert';
+import Card from './components/Card';
 import Header from './components/Header';
-import { Jumbotron, Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -10,14 +9,29 @@ class App extends Component {
       <div>
         <Header />
         <Jumbotron>
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
-        </p>
-      </Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+        <Alert
+          theme="danger"
+          message="pindah ke halaman lain -> "
+          link="http://santrenkoding.org"
+          linkText="Santren Koding"
+        />     
+        {/* <Card
+          title="judul"
+          subTitle="subtitle"
+          text="ini isi textnya"
+          link1="Something"
+          link2="Google"
+          linkText1="https://something.com"
+          linkText2="https://google.com"
+        />  */}
       </div>
     );
   }
