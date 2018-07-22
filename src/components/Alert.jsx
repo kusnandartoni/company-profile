764a23fbae35d7e1c7570ff8ff2ee75e520ba51a
kusnandartoni <kusnandartoni@gmail.com>
2018-07-22 14:50:20 +0700

    fix classname

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -4,8 +4,8 @@ class Alert extends Component {
     render() {
         let type = `alert alert-${this.props.theme}`
         return (
-        <div class={type} role="alert">
-            {this.props.message} <a href={this.props.link} class="alert-link">{this.props.linkText}</a>
+        <div className={type} role="alert">
+            {this.props.message} <a href={this.props.link} className="alert-link">{this.props.linkText}</a>
         </div>
         );
     }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Card extends Component {
     render() {
         return (
-            <div class="card" style={{width: "18rem"}}>
-                <div class="card-body">
-                    <h5 class="card-title">{this.props.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{this.props.subTitle}</h6>
-                    <p class="card-text">{this.props.text}</p>
-                    <a href={this.props.link1} class="card-link">{this.props.linkText1}</a>
-                    <a href={this.props.link2} class="card-link">{this.props.linkText2}</a>
+            <div className="card" style={{width: "18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{this.props.subTitle}</h6>
+                    <p className="card-text">{this.props.text}</p>
+                    <a href={this.props.link1} className="card-link">{this.props.linkText1}</a>
+                    <a href={this.props.link2} className="card-link">{this.props.linkText2}</a>
                 </div>
             </div>
         );
