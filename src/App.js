04c03aae69334c757beb5bf97c02633689f6c1a7
fahmial51 <fahmial51@gmail.com>
2018-07-22 15:21:31 +0700

    tambah spasi

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Header />
         <Jumbotron>
-          <h1 className="display-3">Halo, Dunia!</h1>
+          <h1 className="display-3">Halo,      Dunia!</h1>
           <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
           <hr className="my-2" />
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
