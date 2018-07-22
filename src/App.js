63b5d40a23970d56de06abab0afb9c4549efc154
fahmial51 <fahmial51@gmail.com>
2018-07-22 15:16:53 +0700

    ganti title & hapus alert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Alert from './components/Alert';
+// import Alert from './components/Alert';
 // import Card from './components/Card';;
 import Header from './components/Header';
 import { Jumbotron, Button } from 'reactstrap';
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <Header />
         <Jumbotron>
-          <h1 className="display-3">Hello, world!</h1>
+          <h1 className="display-3">Halo, Dunia!</h1>
           <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
           <hr className="my-2" />
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -18,12 +18,12 @@ class App extends Component {
             <Button color="primary">Learn More</Button>
           </p>
         </Jumbotron>
-        <Alert
+        {/* <Alert
           theme="danger"
           message="pindah ke halaman lain -> "
           link="http://santrenkoding.org"
           linkText="Santren Koding"
-        />     
+        />      */}
         {/* <Card
           title="judul"
           subTitle="subtitle"
